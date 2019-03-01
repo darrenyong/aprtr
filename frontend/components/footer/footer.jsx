2143b0059aa254d83aad53bad2cf8f3f36f93227
darrenyong <contact.darrenyong@gmail.com>
2019-02-28 19:16:22 -0800

    Add mail to link in footer component

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,7 +5,7 @@ export default(props) => {
   return (
     <div className="footer">
       <div className="footer-text">
-        <span class="contact-plug">Contact</span>
+        <span class="contact-plug"><a href="mailto:contact.darrenyong@gmail.com">Contact</a></span>
         <span class="github-plug"><a href="https://github.com/darrenyong">Github</a></span>
         <span class="linkedin-plug"><a href="https://linkedin.com/in/yongdarren">LinkedIn</a></span>
 
