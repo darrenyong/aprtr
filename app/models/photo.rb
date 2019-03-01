2d660704e8d2c2bfd996397a28e7f61b0290c588
darrenyong <contact.darrenyong@gmail.com>
2019-03-01 11:42:09 -0800

    Add association to photo model to test active storage

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id          :bigint(8)        not null, primary key
+#  title       :string
+#  description :text
+#  uploader_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Photo < ApplicationRecord
+  validates :uploader_id, presence: true
+
+  has_one_attached :photo
 end
