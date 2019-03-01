558772cb65f2bbe609890359cef9ee5a5f985738
darrenyong <contact.darrenyong@gmail.com>
2019-02-28 20:28:57 -0800

    Changed class to className in footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,9 +5,9 @@ export default(props) => {
   return (
     <div className="footer">
       <div className="footer-text">
-        <span class="contact-plug"><a href="mailto:contact.darrenyong@gmail.com">Contact</a></span>
-        <span class="github-plug"><a href="https://github.com/darrenyong">Github</a></span>
-        <span class="linkedin-plug"><a href="https://linkedin.com/in/yongdarren">LinkedIn</a></span>
+        <span className="contact-plug"><a href="mailto:contact.darrenyong@gmail.com">Contact</a></span>
+        <span className="github-plug"><a href="https://github.com/darrenyong">Github</a></span>
+        <span className="linkedin-plug"><a href="https://linkedin.com/in/yongdarren">LinkedIn</a></span>
 
       </div>
     </div>
