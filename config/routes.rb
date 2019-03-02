fa09bc9af9aacae458c01f5ec371941233dbeae8
darrenyong <contact.darrenyong@gmail.com>
2019-03-01 16:05:28 -0800

    Add photos backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   root "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:new, :create]
+    resources :users, only: [:new, :create] do
+      resources :photos, only: [:index]
+    end
+
     resource :session, only: [:new, :create, :destroy]
+    resources :photos, only: [:index, :show, :create, :update, :destroy]
   end
 
   resources :photos, only: [:show]
 
-  match "*path", to: redirect('/'), via: :all
+  # match "*path", to: redirect('/'), via: :all
 end
