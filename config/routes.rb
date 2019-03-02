98f12a0e64ccd99b6304388bce95f013ca224746
darrenyong <contact.darrenyong@gmail.com>
2019-03-01 16:18:56 -0800

    Update photo routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
-Rails.application.routes.draw do
-  root "static_pages#root"
+Rails.application.routes.draw do root "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:new, :create] do
+    resources :users, only: [:create] do
       resources :photos, only: [:index]
     end
 
-    resource :session, only: [:new, :create, :destroy]
-    resources :photos, only: [:index, :show, :create, :update, :destroy]
+    resource :session, only: [:create, :destroy]
+    resources :photos, except: [:new, :edit]
   end
-
-  resources :photos, only: [:show]
 
   # match "*path", to: redirect('/'), via: :all
 end
