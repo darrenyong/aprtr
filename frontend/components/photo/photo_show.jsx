390b8942204ec7006b08f49aca03292c638f1887
darrenyong <contact.darrenyong@gmail.com>
2019-03-05 11:46:07 -0800

    Add title and description to Picture Show component

--- frontend/components/photo/photo_show.jsx
+++ frontend/components/photo/photo_show.jsx
@@ -13,15 +13,35 @@ class PhotoShow extends React.Component {
   }
 
   render() {
+    console.log(this.props)
     let photo;
+    let title;
+    let description;
     if (this.props.photo) {
       photo = this.props.photo.photoUrl
+      title = this.props.photo.title
+      description = this.props.photo.description
     }
 
     return (
-      <div className="image-show-parent">
-        <img className="image-show" src={`${photo}`} />
+      <>
+      <div className="imageShow-parent">
+        {/* Need a back to explore page here */}
+        <div className="imageShow-container">
+          <img className="imageShow-photo" src={`${photo}`} />
+          <div className="imageShow-delete">
+              <i className="fas fa-trash-alt" onClick={this.props.deletePhoto(this.props.photo)}></i>
+          </div>
+        </div>
       </div>
+
+      <div className="image-details-parent">
+        <div className="image-details-container">
+          <h1>{title}</h1>
+          <h2>{description}</h2>
+        </div>
+      </div>
+      </>
     );
   }
 }
--- frontend/components/photo/photo_show_container.js
+++ frontend/components/photo/photo_show_container.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import PhotoShow from './photo_show';
-import { fetchPhoto } from '../../actions/photo';
+import { fetchPhoto, deletePhoto } from '../../actions/photo';
 
 const mSP = (state, ownProps) => {
   let photoId = ownProps.match.params.id
@@ -12,7 +12,8 @@ const mSP = (state, ownProps) => {
 
 const mDP = (dispatch) => {
   return ({
-    fetchPhoto: (id) => dispatch(fetchPhoto(id))
+    fetchPhoto: (id) => dispatch(fetchPhoto(id)),
+    deletePhoto: (photo) => dispatch(deletePhoto(photo))
   })
 }
 
