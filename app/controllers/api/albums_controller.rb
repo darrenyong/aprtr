c3d0d4bc8ffe328123aacbccb42177061523a15c
darrenyong <contact.darrenyong@gmail.com>
2019-03-07 15:57:07 -0800

    Add Album index and show jbuilder

---
+++ app/controllers/api/albums_controller.rb
@@ -0,0 +1,56 @@
+class Api::AlbumsController < ApplicationController  
+  def index
+    if params[:user_id]
+      @albums = Album.all.select{ |album| album.user_id.to_s == params[:user_id]}
+      render :index
+    else
+      @albums = nil
+    end
+  end
+
+  def create
+    @album = Album.new(album_params)
+    @album.user_id = current_user.id
+
+    if @album.save
+      render :show
+    else
+      render json: @album.errors.full_messages, status: 500
+    end
+  end
+
+  def show
+    @album = Album.find_by(id: params[:id]) 
+
+    if @album
+      render :show
+    else
+      render json: ["Album not found!"], status: 404
+    end
+  end
+
+  def update
+    @album = current_user.albums.find(params[:id])
+
+    if @album.update(album_params)
+      render :show
+    else
+      render json: ["Album not found!"], status: 404
+    end
+  end
+
+  def destroy
+    @album = current_user.albums.find(params[:id])
+
+    if @album
+      @album.destroy
+    else
+      render json: ["Album not found"], status: 404
+    end
+  end
+
+  private
+  def album_params
+    params.require(:album).permit(:title, :description)
+  end
+end
