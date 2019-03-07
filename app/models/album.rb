42fea1a03e946cfee09e11d561fc89192363b95f
darrenyong <contact.darrenyong@gmail.com>
2019-03-07 11:59:49 -0800

    Add albums table

---
+++ app/models/album.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id          :bigint(8)        not null, primary key
+#  title       :string           not null
+#  description :text
+#  user_id     :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Album < ApplicationRecord
+end
