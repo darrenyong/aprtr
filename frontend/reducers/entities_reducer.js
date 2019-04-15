343982f22562aac9ebcae60e37917dda81f19192
darrenyong <contact.darrenyong@gmail.com>
2019-04-14 21:59:15 -0700

    Add Albums to Entities Reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import photosReducer from "./photo";
+import albumsReducer from "./album"
 import usersReducer from "./user"
 
 export default combineReducers({
   photos: photosReducer,
+  albums: albumsReducer,
   users: usersReducer
 })
