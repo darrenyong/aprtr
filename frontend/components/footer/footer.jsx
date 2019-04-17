b10b5d4aa90e182d2d87378dc45a980a2cb75ca8
darrenyong <contact.darrenyong@gmail.com>
2019-04-16 18:13:12 -0700

    Add portfolio to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -7,7 +7,7 @@ export default(props) => {
         <span className="contact-plug"><a href="mailto:contact.darrenyong@gmail.com">Contact</a></span>
         <span className="github-plug"><a href="https://github.com/darrenyong">Github</a></span>
         <span className="linkedin-plug"><a href="https://linkedin.com/in/yongdarren">LinkedIn</a></span>
-
+        <span className="portfolio-plug"><a href="http://yongdarren.com">Portfolio</a></span>
       </div>
     </div>
   )
